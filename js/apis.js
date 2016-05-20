831a0bb07df42763ac617a47ae39798fea14c3b1
James Wilkinson <jerw@Jamess-MacBook-Pro.local>
2016-05-20 10:26:10 -0400

    cache bust

--- js/apis.js
+++ js/apis.js
@@ -51,15 +51,17 @@ $(document).ready(function() {
           <p>${obj.about}</p>
         </div>`
       );
-      $(".speaker[data-speaker-id='"+obj.id+"']").append(
-        `<div class="speakerBlock">
-          <img src="https://s3.amazonaws.com/eventmobi-assets/eventsbyids/11159/people/${obj.image100}" alt="Headshot"/>
-          <h3 id="-">${obj.first_name}</h3>
-          <h4>${obj.title}&nbsp;at&nbsp;${obj.company_name}</h4>
-          <h4><a class="twitter-follow-button" href="${obj.twitter}" data-show-count="false" data-lang="en">${obj.twitter}</a></h4>
-          <p>${obj.about}</p>
-        </div>`
-      );
+      window.setTimeout(
+        $(".speaker[data-speaker-id='"+obj.id+"']").append(
+          `<div class="speakerBlock">
+            <img src="https://s3.amazonaws.com/eventmobi-assets/eventsbyids/11159/people/${obj.image100}" alt="Headshot"/>
+            <h3 id="-">${obj.first_name}</h3>
+            <h4>${obj.title}&nbsp;at&nbsp;${obj.company_name}</h4>
+            <h4><a class="twitter-follow-button" href="${obj.twitter}" data-show-count="false" data-lang="en">${obj.twitter}</a></h4>
+            <p>${obj.about}</p>
+          </div>`
+        );
+      ,1000);
     });
   });
 });
